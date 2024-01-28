34dfcd44f285486a93bc8f1641680bfe541b65eb
BIZAGoD <157781812+BIZAGoD@users.noreply.github.com>
2024-01-28 10:24:24 -0600

    Add files via upload

--- EMPLEADOS/Operaciones.cs
+++ EMPLEADOS/Operaciones.cs
@@ -1,71 +1,53 @@
 namespace EMPLEADOS;
 
 
-
-abstract class EMPLEADO //CLASE PADRE EMPLEADO
+ public class EMPLEADO //CLASE PADRE EMPLEADO
 {
-    protected double tipoPago;
-    protected double Asala;
-    protected double EmpleXHora; 
-    protected double EmpleXComision;
-    protected double AsalaXComision;
-    protected double respuestaUsuario;
+    public class metodos
+    {}
+    
+    private double tipoPago;
+    private double Asala;
+    private double EmpleXHora; 
+    private double EmpleXComision;
+    private double AsalaXComision;
+    
 
-    public virtual double SueldoAsalario
+    public double Asalariados
     {
-        get;
-        set;
+        get{return Asala;}
+        set{Asala = value;}
     }
-    public virtual double SueldoXHora
+    public double EmpleadosxHORAS
     {
-        get;
-        set;
+        get{return EmpleXHora;}
+        set{EmpleXHora =  value;}
     }
-    public virtual double SueldoEmpleXComi
+    public double EmpleadosXCOMISION
     {
-        get;
-        set;
+        get{return EmpleXComision;}
+        set{EmpleXComision = value;}
     }
-    public virtual double SueldoAsalariXComi
+    public double EmpleadosAsalariadosXCOMISION
     {
-        get;
-        set;
+        get{return AsalaXComision;}
+        set{AsalaXComision = value;}
     }
-    public virtual double respUesuario
-    {
-        get;
-        set;
-    }//<----------------------- Aqui finaliza la clase padre y abajo comienzan las clases hijo o clases heredadas...
 
-    class EmpleadosAsalariados : EMPLEADO
+    public double SUELDOASALARIADOS(double salario)
     {
-        public override double SueldoAsalario { get => base.SueldoAsalario; set => base.SueldoAsalario = value; } //cambia base.SueldoAsalario por la variable correspondiente, sin el base, solamente el nombre de la variable (Asala creo que es)
+        return Asala;
     }
-    class Empleadosxhoras : EMPLEADO
+    public double SUELDOSXHORAS(double EmpleXHora)
     {
-        public override double SueldoXHora { get => base.SueldoXHora; set => base.SueldoXHora = value; } //Lo mismo que la variable anterior
-    }
-    class Empleadosxcomision : EMPLEADO
-    {
-        public override double SueldoEmpleXComi { get => base.SueldoEmpleXComi; set => base.SueldoEmpleXComi = value; } // Cambiar por variables
-    }
-    class Empleadosasalariadosxcomision : EMPLEADO
-    {
-        public override double SueldoAsalariXComi { get => base.SueldoAsalariXComi; set => base.SueldoAsalariXComi = value; } //Cambiar por variables
-    }
-    public class opciones : EMPLEADO
-    {
-        public override double respUesuario { get => base.respUesuario; set => base.respUesuario = value; } //Cambiar por variables
+        return EmpleadosxHORAS;
+
     }
     
-    public class Operaciones 
-    {
-        EmpleadosAsalariados SueldoASALARIOS = new EmpleadosAsalariados();
-        Empleadosxhoras SueldoXHORAS = new Empleadosxhoras();
-        Empleadosxcomision SueldoXCOMISION = new Empleadosxcomision();
-        Empleadosasalariadosxcomision SueldoASALARXCOMISION = new Empleadosasalariadosxcomision();
+    
 
-    }
+ 
+    
 
     
    
--- EMPLEADOS/Program.cs
+++ EMPLEADOS/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace EMPLEADOS;
+﻿using System.Reflection.Emit;
+
+namespace EMPLEADOS;
 
 class Program
 {
+    private double Asala;
     static void Main(string[] args)
 
     {  
+        
+
 
         Console.Clear();
         Console.WriteLine("--------------------------------------------------------------------");
@@ -15,7 +20,29 @@ class Program
         Console.WriteLine(" 2 = Empleado x hora");
         Console.WriteLine(" 3 = Empleado x comision");
         Console.WriteLine(" 4 = Empleado Asalariado x comision");
-        double respuestaUsuario = double.Parse(Console.ReadLine()); //¿Lo hizo Chat GPT?, porque asi declara las variables.
+        double respuesta = double.Parse(Console.ReadLine());
+
+            EMPLEADO obj = new EMPLEADO();
+        if(respuesta == 1)
+        {
+            Console.WriteLine("Ingrese el salario fijo del trabajador");
+            double Asala = double.Parse(Console.ReadLine());
+            
+            double resfinal =obj.SUELDOASALARIADOS(Asala);
+            Console.WriteLine("es este "+resfinal);
+        }
+        if (respuesta== 2)
+        {
+            Console.WriteLine("Ingrese el salario x hora del trabajador");
+            double EmpleXHora = double.Parse(Console.ReadLine());
+            double resfinal2 = obj.SUELDOSXHORAS(EmpleXHora);
+
+            Console.WriteLine("es este otro de: "+resfinal2);
+            
+        }
+
+
+        
 
     }   
 }
